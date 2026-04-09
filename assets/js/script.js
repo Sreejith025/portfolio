acf9a9548b7932ecd24476e8d9737813c416e352
Sreejith <abisri024@gmail.com>
2026-04-09 23:48:55 +0530

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
- // Smooth scroll
-  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener("click", function (e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute("href"))
-        ?.scrollIntoView({ behavior: "smooth" });
-    });
+// Smooth scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href"))
+      ?.scrollIntoView({ behavior: "smooth" });
   });
+});
 
-  document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
 
   const backToTop = document.getElementById("backToTop");
   if (!backToTop) return;
@@ -32,4 +32,46 @@
   });
 
 });
-  
+
+
+const sections = document.querySelectorAll("section");
+const navLinkElements = document.querySelectorAll(".nav-links a");
+const navbar = document.querySelector(".navbar");
+const menuBtn = document.getElementById("menuBtn");
+const navLinksContainer = document.getElementById("navLinks");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 10) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+
+  let current = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    if (window.scrollY >= sectionTop - 200) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinkElements.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === "#" + current) {
+      link.classList.add("active");
+    }
+  });
+});
+
+menuBtn.addEventListener("click", () => {
+  menuBtn.classList.toggle("active");
+  navLinksContainer.classList.toggle("active");
+});
+
+navLinkElements.forEach(link => {
+  link.addEventListener("click", () => {
+    menuBtn.classList.remove("active");
+    navLinksContainer.classList.remove("active");
+  });
+});
