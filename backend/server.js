00b4d0bd9af0a58de9a3adeaca5af5db482e0dc7
Sreejith <abisri024@gmail.com>
2026-04-26 14:30:58 +0530

    fix port for render

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ app.use("/api/reports", require("./routes/reportRoutes"));
 
 app.use("/api/auth", require("./routes/authRoutes"));
 
-app.listen(process.env.PORT || 5000, () => {
-    console.log("🚀 Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
 });
